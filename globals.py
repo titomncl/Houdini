e64bacd907675eae8fea0392d9e8369dc64ae63d
t.nicole <thomas-nicole@hotmail.fr>
2021-06-28 15:32:51 +0200

    Key 'PFE_PROJECT' does not exist.

--- globals.py
+++ globals.py
@@ -3,5 +3,5 @@ import os
 USER_PATH = os.environ['USERPROFILE'].replace('\\', '/')
 PFE_PATH = os.environ["PFE_ENV"]
 DEV_PATH = os.environ["DEV_ENV"]
-PROJECT = os.environ["PFE_PROJET"].split("/")[-1]
 ROOT_PATH = os.environ["ROOT_PATH"]
+PROJECT = PFE_PATH.split("/")[-1]
