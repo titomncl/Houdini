16bfe555ca1e7d68a16aa26a728b907d779ead10
t.nicole <thomas-nicole@hotmail.fr>
2021-11-04 18:04:35 +0100

    🚑️ fix get last file

--- globals.py
+++ globals.py
@@ -7,3 +7,6 @@ PROJECT_PATH = os.environ["PROJECT_ENV"]
 DEV_PATH = os.environ["DEV_ENV"]
 ROOT_PATH = os.environ["ROOT_PATH"]
 PROJECT = PROJECT_PATH.split("/")[-1]
+
+
+FILENAME_PATTERN = r"([A-Z0-9_]+)_([A-Z0-9]+)_(\d{3})"
